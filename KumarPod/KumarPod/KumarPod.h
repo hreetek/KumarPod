9e30c9fe8192d51ae7f3df05fa559c8841e1d248
Kumar Lav <kumarstslav@gmail.com>
2019-09-04 19:39:52 +0530

    my first

---
+++ KumarPod/KumarPod/KumarPod.h
@@ -0,0 +1,19 @@
+//
+//  KumarPod.h
+//  KumarPod
+//
+//  Created by apple on 04/09/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KumarPod.
+FOUNDATION_EXPORT double KumarPodVersionNumber;
+
+//! Project version string for KumarPod.
+FOUNDATION_EXPORT const unsigned char KumarPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KumarPod/PublicHeader.h>
+
+
---
+++ KumarPod/KumarPod/KumarService.swift
@@ -0,0 +1,21 @@
+//
+//  KumarService.swift
+//  KumarPod
+//
+//  Created by apple on 04/09/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import Foundation
+
+
+public class KumarService
+{
+    
+    private init(){}
+    
+    public static func getString()-> String{
+        
+        return "All are done now can use it."
+    }
+}
